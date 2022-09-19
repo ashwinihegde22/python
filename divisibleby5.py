391fadb2b0ff832856d016722b70e80c806d018c
ashwinihegde22 <112920539+ashwinihegde22@users.noreply.github.com>
2022-09-19 04:02:27 -0700

    Add files via upload

---
+++ divisibleby5.py
@@ -0,0 +1,10 @@
+list2=[]
+def divisiable_by_5(list1):
+   print("Given list",list1)
+   for i in x:
+       if i%5==0:
+          list2.append(i)
+
+x=[5,18,155,18,20,25,10]
+divisiable_by_5(x)
+print("The list is",list2)
